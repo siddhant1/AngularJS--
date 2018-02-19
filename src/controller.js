4cf371a77d2f4916f72892d4e2c8d32ec9ebcdfe
siddhant1 <sidhant.manchanda@gmail.com>
2018-02-19 23:32:19 +0530

    AngularJS🔥🔥

--- src/controller.js
+++ src/controller.js
@@ -1,23 +1,18 @@
 app.controller("my-cntrl", function($scope, factory) {
   $scope.id = 1;
   $scope.arr = [];
+  var editObject = {};
   $scope.Display = () => {
-    $scope.arr.forEach(element => {
-      if (element.edited) {
-        element.id = $scope.id;
-        element.name = $scope.name;
-        element.desc = $scope.Desc;
-        element.url = $scope.url;
-      }
-    });
-
+    editObject.checked = true;
+    $scope.Deletebruh();
     var item = new Item($scope.id, $scope.name, $scope.url, $scope.Desc);
     factory.arrayPush(item);
     $scope.arr = factory.arr;
     $scope.id++;
     $scope.name = "";
-    $scope.Desc = "";
     $scope.url = "";
+    $scope.Desc = "";
+    filterBruh();
   };
   $scope.Toggle = id => {
     $scope.arr.forEach(element => {
@@ -27,6 +22,17 @@ app.controller("my-cntrl", function($scope, factory) {
       }
     });
   };
+
+  function filterBruh() {
+    console.log($scope.arr);
+    console.log(factory.arr);
+    factory.arr.sort(function(a, b) {
+      console.log(a);
+      console.log(b);
+      return a.id - b.id;
+    });
+    $scope.arr = factory.arr;
+  }
   $scope.Deletebruh = () => {
     var newArr = [];
     newArr = $scope.arr.forEach(element => {
@@ -34,17 +40,19 @@ app.controller("my-cntrl", function($scope, factory) {
         newArr.push(element);
       }
       $scope.arr = newArr;
+      factory.arr = newArr;
     });
   };
 
-  $scope.Edit = id => {
+  $scope.Edit = object => {
     $scope.arr.forEach(element => {
-      if ((element.id = id)) {
+      if (element.id == object.id) {
         $scope.id = element.id;
         $scope.name = element.name;
         $scope.Desc = element.desc;
         $scope.url = element.url;
         element.edited = true;
+        editObject = object;
       }
     });
   };
